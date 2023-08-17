0e6a3b2fe31cdf8a2a8edcc9ce1155d19d128c63
ashish4323 <ashish.k@fwc.co.in>
2023-08-17 20:05:14 +0530

    Use Effect

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,46 @@
-import {useState} from "react"
+import {useState,useEffect} from "react"
 import './App.css';
 import TodoCard from "./components/TodoCard";
 function App() {  
 
-  const [todos,setTodos] =useState([])
+  const [todos,setTodos] = useState(null)
+
   // state to store task and description input values
   const [task,setTaks] = useState("")
   const [description,setDescription] = useState("")
 
+  useEffect(() => {
+    let t
+    if(localStorage.getItem("todos"))
+    {
 
+       t =  JSON.parse(localStorage.getItem('todos'))
+    }
+    else
+    {
+      t = []
+    }
 
+    setTodos(t)
+  },
+  [])
+
+  useEffect(() => {
+    if(todos != null)
+      localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
+  function addTodo()
+  {
+    let todo = {
+      task: task,
+      description: description
+    }
+    // [1]
+    setTodos([...todos,todo])
+    
+    
+  }
 
   return (
     <div className="App">
@@ -19,11 +50,11 @@ function App() {
         <br />
         <input onChange={e => setDescription(e.currentTarget.value)} type="text" placeholder="Enter Description"/>
         <br />
-        <button>Add Todo</button>
+        <button onClick={addTodo}>Add Todo</button>
       </div>
 
       <div >
-      {todos.map(item => 
+      {todos && todos.map(item => 
         <TodoCard task={item.task} description={item.description} />
       )}
       </div>
