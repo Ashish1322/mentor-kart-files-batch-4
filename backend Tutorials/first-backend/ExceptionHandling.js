40cd37fe995f453c85f1ed9e6ba4e9cd52a0b740
ashish4323 <ashish.k@fwc.co.in>
2023-09-04 22:34:52 +0530

    Error Handling + MongoDb Commands

---
+++ backend Tutorials/first-backend/ExceptionHandling.js
@@ -0,0 +1,14 @@
+// Exception handling is the proccess of handling the errors that may occur during runtime of your program
+
+try
+{
+    console.log("HI")
+    console.lg("We are learning Exception Handling")
+}
+catch (err)
+{
+    console.log(err.message)
+}
+
+console.log("Finish")
+
---
+++ backend Tutorials/first-backend/mongodbCommands.js
@@ -0,0 +1,26 @@
+// MongoDb Commands
+
+// 1. Show dbs ==> To show all the databases 
+// 2. Use databasename ==> Create a new database with the given name and then switch to that
+// 3. Db ==> To show the current working database
+// 4. Show collections ==> Show the collections of the current database
+// 5. db.createCollection(‘name’) ==> Create a new collection in the present database
+// 6. db.collectionname.drop() ==> Delete the given collection from the current database.
+
+
+// Mongo Db Commands for working with documents (Rows) in the given collection of the current database.
+// 1. Db.<collectionName>.insert( {object} ) ==> Insert the given document in the given collection
+// 2. Db.<collectionName>.insertMany(  [ Array of  Objects ]   ) => Insert the given documents in the given collection
+// 3. Db.<collectionName>.find() ==> Returns all the documents in the collection
+// 4. Db.<collectionName>.find().pretty() => Returns all the documents in the pretty format.
+// 5. Db.<collectionName>.find().limit(no) ==> Limit the results with given numbers 
+// 6. Db.<collectionName>.find().count() ==> Returns the count of output
+// 7. Db.<collectionName>.find().sort( {}  ) ==> Sorts on the basis of given object
+// 8. Db.<collectionName>.findOne( {} ) ==> Find the first row matching with object
+// 9. Db.<collectionName>.update( Matching object, Updated Object). ==> To change the matching object with updated
+// 10. Db.<collectionName>.remove( matching object ) ==> Remove the mathing object
+
+
+
+
+
