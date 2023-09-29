9930efec56153c9991d8ca28062f175adf4e0674
ashish4323 <ashish.k@fwc.co.in>
2023-09-29 23:40:08 +0530

    Multer UPload Files

---
+++ backend Tutorials/fileupload/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "files");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "ABCD" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage, limits: { fileSize: 1024 } });
+
+app.post("/upload-file", upload.single("resume"), function (req, res) {
+  console.log(req.body);
+  console.log(req.file);
+
+  res.send("File Uplaoded");
+});
+
+app.listen(3002, () => console.log("Server is Running"));
