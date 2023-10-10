3addd339c70d136326c91548a07344d3408a4093
ashish4323 <ashish.k@fwc.co.in>
2023-10-10 22:38:47 +0530

    Database Normalisation

---
+++ chat application/backend/modals/Friends.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const friendSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Accepted", "Rejected"],
+    default: "Pending",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Friends", friendSchema);
---
+++ chat application/backend/modals/Messages.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  reciever: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  messageId: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Messages", messageSchema);
